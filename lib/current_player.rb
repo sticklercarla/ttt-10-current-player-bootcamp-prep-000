4d38a656cf4621e0a6dad45b9f58cf8ad5c373b9
Carla Stickler <carlastickler@gmail.com>
2019-09-18 01:07:51 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,10 @@
 require 'pry'
 
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+  binding.pry
+end
+
 def turn_count(board)
   turns = 0 
   board.each do |token|
@@ -8,9 +13,4 @@ def turn_count(board)
     end
   end
   token
-end
-
-def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
-  binding.pry
 end
